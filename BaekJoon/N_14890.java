039c4e37ead4d13640a22a468fd380303e333784
PARKYOONJUNG <dbsdl315@gmail.com>
2020-02-27 20:10:02 +0900

    경사로
    경사로

--- BaekJoon/N_14890.java
+++ BaekJoon/N_14890.java
@@ -1,115 +1,102 @@
-package SSAFY;
+package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class N_14890 {
-	public static void main(String[] args) throws IOException {
+	static int[][] arr;
+	static int X;
+	static int result=0;
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[N][N];
-		for (int i = 0; i < arr.length; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < arr[i].length; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 한변의 크기
+		X = sc.nextInt(); // 경사로의 길이
+		arr = new int[N][N];
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				arr[i][j] = sc.nextInt();
 			}
 		}
-		
-		int answer = 0;
-		for (int i = 0; i < arr.length; i++) { // 행
-			int size = arr[i][0]; // 현재 칸의 높이
-			int dx = i;
-			int dy = 1;
-			int count = 1; // 오르막길은 오르막길 판단 전 경사로 가능한 개수 세어야함 이를 위한 변수
-			boolean check = true; // 경사로 세우는 것이 불가능한 경우를 체크하기 위한 변수
-			while(true) { 
-				if(dy >= arr[0].length || Math.abs(size-arr[dx][dy]) > 1) break;
-				
-				if(size > arr[dx][dy]) { // 내리막길	
-					int temp_size = arr[dx][dy];
-					int temp_count = 1;
-					int ty = dy+1; // 다음 행
-					while(true) {
-						if(ty >= arr[0].length || temp_size != arr[dx][ty]) break;
-						ty+=1; // 다음 열
-						temp_count++;
-					}
-					if(temp_count < L || (dy+L < arr[0].length && temp_size-arr[dx][dy+L] < 0)) {
-						check = false;
-						break;
-					}
-					size = temp_size; // 경사로 세운 칸의 값
-					count = temp_count-L; // 사용할 수 있는 경사로 개수에서 사용한 만큼을 뺀 개수
-					dy = ty; // 다음 칸의 위치						
-				}else if(size < arr[dx][dy]) { // 오르막길
-					if(size != arr[dx][dy] && count < L) { 
-						check = false;
-						break;
-					}
-					size = arr[dx][dy]; // 다음 칸의 값
-					count = 1; // 개수 1
-					dy+=1; // 다음 행
-
-				}else { // 같은 길
-					count++;
-					dy+=1; // 다음 행
-				}
-			}
-			
-			if(check && dy >= arr[0].length) answer++;
-		}
-		
-		for (int i = 0; i < arr[0].length; i++) { // 열
-			int size = arr[0][i];
-			int dx = 1;
-			int dy = i;
-			int count = 1;
-			boolean check = true;
-			while(true) {
-				if(dx >= arr.length || Math.abs(size-arr[dx][dy]) > 1) break;
-				
-				if(size > arr[dx][dy]) { // 내리막길
-					int temp_size = arr[dx][dy];
-					int temp_count = 1;
-					int tx = dx+1; 
-					while(true) {
-						if(tx >= arr.length || temp_size != arr[tx][dy]) break;
-						tx+=1; // 다음 행
-						temp_count++;
-					}
-					if(temp_count < L || (dx+L < arr.length && temp_size-arr[dx+L][dy] < 0)) {
-						check = false;
-						break;
-					}
-					size = temp_size;						
-					count = temp_count-L;
-					dx = tx;
-				}else if(size < arr[dx][dy]){ // 오르막길
-					if(size != arr[dx][dy] && count < L) { // 경사로 가능한 지 판단
-						check = false;
-						break;
-					}
-					size = arr[dx][dy]; // 다음 칸의 값
-					count = 1;	
-					dx+=1; // 다음 열
-				}else { // 같은 길
-					count++;
-					dx+=1; // 다음 열
-				}
-			}
-			
-			if(check && dx >= arr.length) answer++;
-		}
-		
-		System.out.println(answer);
+		checkRow();
+		checkCol();
+		System.out.println(result);
 	}
 	
+	public static void checkRow() {
+		for(int i=0;i<arr.length;i++) {
+			boolean check = true;
+			int num = arr[i][0];
+			int count = 1;
+			int use = 0;
+			for(int j=1;j<arr[0].length;j++) {
+				if(arr[i][j]-num == -1) { // 3 >> 2
+					int temp = 0;
+					num = arr[i][j];
+					count = 1;
+					for(int k=j;k<arr[0].length;k++) {
+						if(arr[i][k] != num) break; 
+						temp++;
+					}
+					if(temp < X) {
+						check = false;
+						break;
+					}else {
+						use = num;
+					}
+				}else if(arr[i][j]-num == 1) { // 2 >> 3
+					if(count < X || (num == use && count - X < X)) {
+						check = false;
+						break;
+					}
+					num = arr[i][j];
+					count = 1;
+				}else if(arr[i][j] == num){ // 2 == 2
+					count++;					
+				}else if(Math.abs(arr[i][j] - num) != 1) {
+					check = false;
+					break;
+				}
+			}
+			if(check) result++;
+		}
+	}
 	
-	
-
+	public static void checkCol() {
+		for(int i=0;i<arr[0].length;i++) {
+			boolean check = true;
+			int num = arr[0][i];
+			int count = 1;
+			int use = 0;
+			for(int j=1;j<arr.length;j++) {
+				if(arr[j][i]-num == -1) { // 3 >> 2
+					int temp = 0;
+					num = arr[j][i];
+					count = 1;
+					for(int k=j;k<arr.length;k++) {
+						if(arr[k][i] != num) break; 
+						temp++;
+					}
+					if(temp < X) {
+						check = false;
+						break;
+					}else {
+						use = num;
+					}
+				}else if(arr[j][i]-num == 1) { // 2 >> 3
+					if(count < X || (num == use && count - X < X)) {
+						check = false;
+						break;
+					}
+					num = arr[j][i];
+					count = 1;
+				}else if(arr[j][i] == num){ // 2 == 2
+					count++;					
+				}else if(Math.abs(arr[j][i] - num) != 1) {
+					check = false;
+					break;
+				}
+			}
+			if(check) result++;
+			}
+		}
 }
