47196c69e120ec0d3def349e209e98dbda855fe2
PARKYOONJUNG <dbsdl315@gmail.com>
2020-04-16 15:55:57 +0900

    2*n 타일링
    2*n 타일링

---
+++ BaekJoon/N_11726.java
@@ -0,0 +1,26 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class N_11726 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] memo = new int[N+1];
+		memo[1] = 1;		
+		if(N == 2) {
+			memo[1] = 1;
+			memo[2] = 2;		
+		}else if(N > 2){
+			memo[1] = 1;
+			memo[2] = 2;	
+			for (int i = 3; i <= N; i++) {
+				memo[i] = (memo[i-2] + memo[i-1]) % 10007;
+			}
+		}
+		System.out.println(memo[N]);
+	}
+
+}
