aea48eb8a5604bb81641f037a58cad42a24a473e
PARKYOONJUNG <dbsdl315@gmail.com>
2020-04-19 21:46:48 +0900

    계단 오르기
    계단 오르기

--- BaekJoon/N_2579.java
+++ BaekJoon/N_2579.java
@@ -3,32 +3,32 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class N_2579 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] score = new int[n];
-		for (int i = 0; i < n; i++) {
+		int N = Integer.parseInt(br.readLine());
+		int[] score = new int[N+1];
+		for (int i = 1; i <= N; i++) {
 			score[i] = Integer.parseInt(br.readLine());
 		}
 		
-		
-		int[] stair = new int[n];
-		stair[0] = score[0];
-		if(n > 1) { // n이 1보다 작은 경우 처리
-			stair[1] = Math.max(stair[0]+score[1], score[1]);
-		}
-		if(n > 2) { // n이 2보다 작은 경우 처리
-			stair[2] = Math.max(score[0]+score[2], score[1]+score[2]);
-			for (int i = 3; i < n; i++) {
-				stair[i] = Math.max(stair[i-2]+score[i], stair[i-3]+ score[i-1] + score[i]);				
+		int[] memo = new int[N+1];
+		if(N == 1) {
+			memo[1] = score[1];
+		}else if(N==2) {
+			memo[1] = score[1];
+			memo[2] = Math.max(score[1]+score[2], score[2]);
+		}else {
+			memo[1] = score[1];
+			memo[2] = Math.max(score[1]+score[2], score[2]);
+			memo[3] = Math.max(score[1]+score[3], score[2]+score[3]);
+			for (int i = 4; i <= N; i++) {
+				memo[i] = Math.max(memo[i-3]+score[i-1]+score[i], memo[i-2]+score[i]);
 			}
 		}
-		
-		System.out.println(stair[n-1]);
+		System.out.println(memo[N]);
 	}
 
 }
